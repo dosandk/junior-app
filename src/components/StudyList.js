1db4431ad52d3aa54a99cb71acfb0c6da2d364f8
Yuliana Pankiv <yuppie.julia@gmail.com>
2018-10-02 17:21:31 +0300

    refactored the child components, added back button

--- src/components/StudyList.js
+++ src/components/StudyList.js
@@ -1,23 +1,19 @@
 import React , { Component } from 'react';
 import '../styles/General.css';
-import '../styles/StudyList.css'
+import '../styles/StudyList.css';
 import {studyTopics} from '../constants/URLs';
 import Subtitles from './SubtitlesList';
-import SubtitleText from './SubtitleText';
 
 export default class StudyList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            subtitlesListHidden: true, //should be a bool
             topicsJunior: null, //should be an array
-            subtitleSelected: false, //should be a bool
             subtitles: null, //should be an array
-            subtitleText: null //should be a string
+            subtitlesShown: false
         };
         this.clickHeader = this.clickHeader.bind(this);
         this.clickTitle = this.clickTitle.bind(this);
-        this.clickSubtitle = this.clickSubtitle.bind(this);
     }
 
     componentWillMount() {
@@ -35,7 +31,6 @@ export default class StudyList extends Component {
     clickHeader() {
         this.setState({
             subtitles: null,
-            subtitleText: null
         });
     }
 
@@ -43,24 +38,9 @@ export default class StudyList extends Component {
         let index = this.state.topicsJunior.category.map(el => {return el.title}).indexOf(e.target.innerHTML);
         let subtitles = this.state.topicsJunior.category[index].subtitles;
         this.setState({
-            subtitlesListHidden: false,
             subtitles: subtitles,
-            subtitleText: null
+            subtitlesShown: true
         });
-    }
-
-    clickSubtitle(e) {
-        let index = this.state.subtitles.map(el => {return el.item}).indexOf(e.target.innerHTML);
-        let url = this.state.subtitles[index].url;
-        fetch(url)
-            .then(res => {
-                return res.json();
-            })
-            .then(data => {
-                this.setState({
-                    subtitleText: data.text
-                });
-            });
     }
 
     render() {
@@ -73,8 +53,7 @@ export default class StudyList extends Component {
             <h1 className="header" onClick={this.clickHeader}>Welcome back, Junior!</h1>
                 <div className="table">
                     {titles}
-                    {this.state.subtitles ? <Subtitles subtitles={this.state.subtitles} clickSubtitle={this.clickSubtitle}/> : null}
-                    {this.state.subtitleText ? <SubtitleText text={this.state.subtitleText}/> : null}
+                    {this.state.subtitles ? <Subtitles subtitles={this.state.subtitles} subtitlesShown={this.state.subtitlesShown}/> : null}
                 </div>
         </div>;
     }
--- src/components/SubtitlesList.js
+++ src/components/SubtitlesList.js
@@ -1,11 +1,53 @@
-import React from "react";
+import React, { Component } from "react";
+import SubtitleText from './SubtitleText';
+import '../styles/General.css';
 
-export default function Subtitles(props) {
-    return <div className="list">
-        <dl className="dl-style">
-            {props.subtitles.map(
-                item => <dt className="dt-style" key={item.item} onClick={props.clickSubtitle}>{item.item}</dt>
-            )}
-        </dl>
-    </div>
+export default class Subtitles extends Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+            subtitleText: null,
+            subtitleListShown: this.props.subtitlesShown
+        };
+        this.clickSubtitle = this.clickSubtitle.bind(this);
+        this.clickBtn = this.clickBtn.bind(this);
+    }
+
+    clickBtn() {
+        this.setState({
+            subtitleText: null,
+            subtitleListShown: !this.state.subtitleListShown
+        })
+    }
+
+    clickSubtitle(e) {
+        let index = this.props.subtitles.map(el => {return el.item}).indexOf(e.target.innerHTML);
+        let url = this.props.subtitles[index].url;
+        fetch(url)
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                this.setState({
+                    subtitleText: data.text,
+                    subtitleListShown: !this.state.subtitleListShown
+                });
+            });
+    }
+
+    render() {
+
+        const list = this.state.subtitleListShown ? <div className="list">
+            <dl className="dl-style">
+                {this.props.subtitles.map(
+                    item => <dt className="dt-style" key={item.item} onClick={this.clickSubtitle}>{item.item}</dt>
+                )}
+            </dl>
+        </div> : <button className="button" onClick={this.clickBtn}>Go back</button>;
+
+        return <div>
+            {list}
+            {this.state.subtitleText ? <SubtitleText text={this.state.subtitleText}/> : null}
+        </div>
+    }
 }
